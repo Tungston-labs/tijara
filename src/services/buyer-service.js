b83f3f8e331d96836ce5664ba8bf580eb626feda
Ajay-tungston <you@example.com>
2025-06-03 16:32:38 +0530

    registration screen is in progress

--- src/services/buyer-service.js
+++ src/services/buyer-service.js
@@ -1,13 +1,13 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 import API from "./config";
 
-API.interceptors.request.use(async (config) => {
-  const token = await AsyncStorage.getItem("accessToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// API.interceptors.request.use(async (config) => {
+//   const token = await AsyncStorage.getItem("accessToken");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
 export const buyerSignUp = async (payload) => {
   const response = await API.post("/buyer/buyer-sign-up", payload);
--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 // api.js
 import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API = axios.create({
   withCredentials: true,
@@ -11,12 +11,12 @@ const API = axios.create({
 });
 
 // Add token dynamically via request interceptor
-API.interceptors.request.use(async (config) => {
-  const token = await AsyncStorage.getItem("accessToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// API.interceptors.request.use(async (config) => {
+//   const token = await AsyncStorage.getItem("accessToken");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
 export default API;
