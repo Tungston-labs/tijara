4ff2348fef713d2757f507264f86800b1c949525
Ajay-tungston <you@example.com>
2025-07-01 12:18:17 +0530

    Package dependancy conflict fixed for some unwanted packages

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Provider } from "react-redux"; 
 
-import MainNavigation from "./src/navigation/navigation";
+import MainNavigation from "./src/navigation/navigation.js";
 import { enableScreens } from "react-native-screens";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { store } from "./src/redux/store";
+import { store } from "./src/redux/store.js";
 
 enableScreens();
 
