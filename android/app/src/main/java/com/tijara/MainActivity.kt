f244c20b134e4e872b436da575e14fe57bfd92bd
Tungston-labs <support@tungstonlabs.com>
2025-11-20 15:39:02 +0530

    working on fetching the location

--- android/app/src/main/java/com/tijara/MainActivity.kt
+++ android/app/src/main/java/com/tijara/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.thijara
 
+import android.os.Bundle // Import Bundle if it's not already there
+import androidx.activity.enableEdgeToEdge // 👈 1. IMPORT THIS
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,16 +9,22 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "tijara"
+    // You MUST override onCreate() to call enableEdgeToEdge()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 2. Call enableEdgeToEdge() BEFORE super.onCreate()
+        enableEdgeToEdge() 
+        
+        super.onCreate(savedInstanceState)
+    }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     */
+    override fun getMainComponentName(): String = "tijara"
+
+    /**
+     * Returns the instance of the [ReactActivityDelegate].
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
